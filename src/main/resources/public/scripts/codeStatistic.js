cd0c48d4a74ad4a69bc36b23a07c23da7b44e78e
AlexCarp <sk6de6iv6@gmail.com>
2016-06-26 23:51:59 +0300

    Add second chart: total people. Upgrade some bugs. Return to creating charts from client object for awhile.

--- src/main/resources/public/scripts/codeStatistic.js
+++ src/main/resources/public/scripts/codeStatistic.js
@@ -1,56 +1,50 @@
-var buttons  = document.getElementById("buttons");
-var regMonthChart = document.getElementById("chart_div");
-var statisticArr;
+window.onload = function () {
+    var buttons  = document.getElementById("buttons");
 
-/*var client = {
- "client":
- {"regMonth":
- {"2015-11":"2","2015-10":"1","2016-3":"5","2016-2":"4","2016-5":"3","2016-4":"1","2015-12":"1"},
+    /*var request = new XMLHttpRequest();
+     var url = "StatisticController";
 
- "knowFrom":
- {"":"11","redica":"1","друг":"2","интернет":"4","jill":"1"},
+     request.onreadystatechange = function() {
+     if (request.readyState == 4 && request.status == 200) {
+     var statisticArr = JSON.parse(request.responseText);
+     }
 
- "totalPeoples":
- {"all":19,"ж":4,"м":15},
+     showRegMonthChart(statisticArr);
+     };
 
- "genderAge":
- {"W18_21":0,"W24_27":0,"W21_24":2,"M21_24":2,"M27_30":3,"M24_27":3,"M45":0,"W27_30":0,"M18":2,"W30_35":1,"M18_21":0,"W45":1,"W35_45":0,"M30_35":5,"M35_45":0,"W18":0}},
+     request.open("GET", url, true);
+     request.send();*/
 
- "subscription":
- {"popSubscr":
- {"Абонемент безлимитный":"2","Групповые занятия 8 раза в месяц":"1","4 посещения в любое время":"5","Групповые занятия 4 раза в месяц":"2","Абонемент 'Хан-Тенгри'":"1"}
- }
- };*/
+    buttons.onclick = function (event) {
+        var target = event.target;
+        if (target.tagName != "A") return;
+        hideAndShowCharts(target.getAttribute("id"));
+    };
+};
 
-var request = new XMLHttpRequest();
-var url = "StatisticController";
+var client = {
+    "client":
+    {"regMonth":
+    {"2015-11":"2","2015-10":"1","2016-3":"5","2016-2":"4","2016-5":"3","2016-4":"1","2015-12":"1"},
 
-request.onreadystatechange = function() {
-    if (request.readyState == 4 && request.status == 200) {
-        statisticArr = JSON.parse(request.responseText);
+        "knowFrom":
+        {"":"11","redica":"1","друг":"2","интернет":"4","jill":"1"},
+
+        "totalPeoples":
+        {"all":19,"ж":4,"м":15},
+
+        "genderAge":
+        {"W18_21":0,"W24_27":0,"W21_24":2,"M21_24":2,"M27_30":3,"M24_27":3,"M45":0,"W27_30":0,"M18":2,"W30_35":1,"M18_21":0,"W45":1,"W35_45":0,"M30_35":5,"M35_45":0,"W18":0}},
+
+    "subscription":
+    {"popSubscr":
+    {"Абонемент безлимитный":"2","Групповые занятия 8 раза в месяц":"1","4 посещения в любое время":"5","Групповые занятия 4 раза в месяц":"2","Абонемент 'Хан-Тенгри'":"1"}
     }
-
-    showRegMonthChart(statisticArr);
 };
-
-request.open("GET", url, true);
-request.send();
-
-buttons.onclick = function (event) {
-    var target = event.target;
-    if (target.tagName != "A") return;
-    hideAndShowCharts(target.getAttribute("id"));
-};
-
 
 function showRegMonthChart(statisticArr) {
-    var regMonth = [['Дата', 'Количество зарегистрировавшихся']];
-    for (key in statisticArr.client.regMonth) {
-        var curent = [];
-        curent.push(key);
-        curent.push(+statisticArr.client.regMonth[key]);
-        regMonth.push(curent);
-    }
+    var regMonth = makeDataArr(statisticArr.client.regMonth);
+    regMonth.unshift(['Дата', 'Количество зарегистрировавшихся']);
 
     google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawChart);
@@ -63,18 +57,76 @@ function showRegMonthChart(statisticArr) {
             vAxis: {minValue: 0}
         };
 
-        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div_reg_month'));
         chart.draw(data, options);
     }
 }
 
+function showTotalPeopleChart(statisticArr, height, width) {
+    var totalPeople = makeDataArr(statisticArr.client.totalPeoples);
+    totalPeople.unshift(['', '']);
+
+    google.charts.setOnLoadCallback(drawBasic);
+
+    function drawBasic() {
+
+        var data = google.visualization.arrayToDataTable(
+            totalPeople
+            /*[
+             ['', '', {role: "style"}],
+             ['Men', 175, '#7CD9FF'],
+             ['Women', 80, '#FF4BED'],
+             ['Total', 255, '#ffff00']
+             ]*/);
+
+        var options = {
+            title: 'Amount of members',
+            chartArea: {width: '50%'},
+            hAxis: {
+                title: 'Number of members',
+                minValue: 0
+            },
+            vAxis: {
+                title: ''
+            },
+            width: width,
+            height: height,
+            legend: { position: "none" }
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div_total_people'));
+
+        chart.draw(data, options);
+    }
+}
 function hideAndShowCharts(stringId) {
+    var regMonthChart = document.getElementById("chart_div_reg_month");
+    var totalPeopleChart = document.getElementById("chart_div_total_people");
     if (stringId != "regMonth") {
-        $(regMonthChart).slideUp(500);
+        var chartDiv = document.getElementById("chart_div_reg_month");
+        var height = chartDiv.offsetHeight;
+        var width = chartDiv.offsetWidth;
+        showTotalPeopleChart(client, height, width);
+        $(regMonthChart).hide(350);
+        $(totalPeopleChart).show(350);
     } else {
-        $(regMonthChart).slideDown(500);
+        $(totalPeopleChart).hide(350);
+        $(regMonthChart).show(350);
     }
     //Необходимо доработать, когда будут все графики
 }
 
+function makeDataArr(statisticArr) {
+    var dataArr = [];
+    for (var key in statisticArr) {
+        if (statisticArr.hasOwnProperty(key)) {
+            var current = [];
+            current.push(key);
+            current.push(+statisticArr[key]);
+            dataArr.push(current);
+        }
+    }
+    return dataArr;
+}
 
+showRegMonthChart(client);
