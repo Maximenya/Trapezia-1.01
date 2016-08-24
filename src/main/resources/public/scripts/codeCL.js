f462b7efc7b850012181489d728d3e35d423b505
AlexCarp <sk6de6iv6@gmail.com>
2016-08-24 23:12:15 +0300

    Rewrite function for disabling button on CL page.

--- src/main/resources/public/scripts/codeCL.js
+++ src/main/resources/public/scripts/codeCL.js
@@ -1,11 +1,17 @@
-/**
- * Created by Denis on 12.01.2016.
- */
-
 window.onload = function () {
     var button = document.getElementById('button');
+    var input = document.getElementById('search');
+    input.onkeyup = function() {
+        if (this.value.length > 0) {
+            button.disabled = false;
+        }
+        else {
+            button.disabled = true;
+        }
+    };
     button.onclick = function() {
             var  str = document.getElementById('search').value;
             var clientId = str.replace(/\D/g, '');
             window.location.href='/visit?clientId='+clientId;
-        };};
+    };
+};
